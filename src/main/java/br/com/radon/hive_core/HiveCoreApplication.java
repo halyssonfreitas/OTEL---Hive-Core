74ac59dd691fc7663573ee47be439b1b58db871c
Halysson <halyssonfreitas@gmail.com>
2025-06-12 07:52:45 -0300

    :tada: start project

---
+++ src/main/java/br/com/radon/hive_core/HiveCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.radon.hive_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/radon/hive_core/HiveCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.radon.hive_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
